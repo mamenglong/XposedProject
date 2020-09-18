655cce0b86655863dc05bed5d21c4cdc3b5f6e44
menglong <mml2015@126.com>
2020-09-18 18:41:15 +0800

    .

--- app/src/main/java/com/ml/xposedproject/MainActivity.kt
+++ app/src/main/java/com/ml/xposedproject/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ml.xposedproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import dalvik.system.PathClassLoader
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
@@ -11,18 +12,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         tv_text.text = getInfo()
+        assets.open("classes.dex").use {input->
+            val file = File(externalCacheDir,"classes.dex")
+            if (file.exists()){
+                file.delete()
+            }
+            file.outputStream().use {
+                input.copyTo(it)
+                Toast.makeText(this,"复制成功",Toast.LENGTH_SHORT).show()
+            }
+        }
         kotlin.runCatching {
-            val file = File(packageCodePath)
+            val file = File(externalCacheDir,"classes.dex")
             val pathClassLoader =
                 PathClassLoader(file.absolutePath, ClassLoader.getSystemClassLoader())
-            val cls= Class.forName(XposedInit::class.java.name, true, pathClassLoader)
-            val instance = cls.newInstance()
+            val cls= Class.forName(MainActivity::class.java.name, true, pathClassLoader)
+            val instance = cls.newInstance() as MainActivity
+            tv_text.text = instance.getInfo1()
         }.onFailure {
             it.printStackTrace()
         }
 
     }
     fun getInfo(): String {
-        return "hahahha"
+        return "hahahha11"
+    }
+    fun getInfo1(): String {
+        return "hahahha11"
     }
 }
